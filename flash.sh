77d86257202a8f00c365d8d3c8cc0a38c230f9aa
Jan Romann <jan.romann@uni-bremen.de>
2023-07-08 18:58:27 +0200

    fixup! Add flashing script

--- flash.sh
+++ flash.sh
@@ -7,7 +7,7 @@ make -f tensorflow/lite/micro/tools/make/Makefile TARGET=sparkfun_edge hello_wor
 # Sign the binary
 python3 tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/tools/apollo3_scripts/create_cust_image_blob.py --bin tensorflow/lite/micro/tools/make/gen/sparkfun_edge_cortex-m4/bin/hello_world.bin --load-address 0xC000 --magic-num 0xCB -o main_nonsecure_ota --version 0x0
 
-python3 tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/tools/apollo3_scripts/create_cust_wireupdate_blob.py --load-address 0x20000 --bin main_nonsecure_ota.bin -i 6 -o main_nonsecure_wire --options 0x1^C
+python3 tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/tools/apollo3_scripts/create_cust_wireupdate_blob.py --load-address 0x20000 --bin main_nonsecure_ota.bin -i 6 -o main_nonsecure_wire --options 0x1
 
 # Flash the binary
 python3 tensorflow/lite/micro/tools/make/downloads/AmbiqSuite-Rel2.2.0/tools/apollo3_scripts/uart_wired_update.py -b ${BAUD_RATE} ${DEVICENAME} -r 1 -f main_nonsecure_wire.bin -i 6
