b1ea75f72c2ab48ff52715729e8bfa046ae84b6f
Jan Romann <jan.romann@uni-bremen.de>
2023-07-12 18:07:52 +0200

    Add code for assignment 10

--- tensorflow/lite/micro/examples/person_detection/sparkfun_edge/detection_responder.cc
+++ tensorflow/lite/micro/examples/person_detection/sparkfun_edge/detection_responder.cc
@@ -23,6 +23,58 @@ limitations under the License.
 
 #include "am_bsp.h"  // NOLINT
 
+// Constant indicating the difference required for a decision for the person
+// or no-person case.
+const int16_t CONFIDENCE_THRESHOLD = 32;
+
+// Turns off all LEDs (except the blue one).
+void ResetLeds() {
+  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
+  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_GREEN);
+  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_RED);
+}
+
+// Prints out a debug debug header using the two original scores.
+void PrintDebugHeader(tflite::ErrorReporter* error_reporter,
+                        uint8_t person_score, uint8_t no_person_score) {
+  TF_LITE_REPORT_ERROR(error_reporter, "===================================");
+  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
+                       person_score, no_person_score);
+}
+
+// Differentiate between the three possible cases using the confidence threshold:
+// If the difference between the two scores is higher than the threshold,
+// turn the respective LED on (green for person, yellow for no person).
+// If the difference is lower than the confidence threshold, turn on the red LED.
+//
+// Besides the turning on an LED, the function also prints out debug messages,
+// indicating the concrete values using for the computation.
+void ProcessPrediction(tflite::ErrorReporter* error_reporter, int16_t prediction) {
+  if (prediction > CONFIDENCE_THRESHOLD) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Person identified: Flash green LED");
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_GREEN);
+  } else if (prediction < -CONFIDENCE_THRESHOLD) {
+    TF_LITE_REPORT_ERROR(error_reporter, "No Person identified: Flash yellow LED");
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
+  } else {
+    TF_LITE_REPORT_ERROR(error_reporter, "Identification ambigious: Flash red LED");
+    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_RED);
+  }
+}
+
+// Create an absolute value from the prediction for debug logging.
+int16_t GetAbsolutePrediction(int16_t prediction) {
+  return prediction >= 0 ? prediction : -prediction;
+}
+
+// Print a debug footer with the confidence value and the confidence threshold.
+void PrintDebugFooter(tflite::ErrorReporter* error_reporter, int16_t prediction) {
+  int16_t absolute_prediction = GetAbsolutePrediction(prediction);
+
+  TF_LITE_REPORT_ERROR(error_reporter, "Confidence value: %d", absolute_prediction);
+  TF_LITE_REPORT_ERROR(error_reporter, "Confidence threshold: %d", CONFIDENCE_THRESHOLD);
+}
+
 // This implementation will light up LEDs on the board in response to the
 // inference results.
 void RespondToDetection(tflite::ErrorReporter* error_reporter,
@@ -40,18 +92,18 @@ void RespondToDetection(tflite::ErrorReporter* error_reporter,
   // Toggle the blue LED every time an inference is performed.
   am_devices_led_toggle(am_bsp_psLEDs, AM_BSP_LED_BLUE);
 
-  // Turn on the green LED if a person was detected.  Turn on the yellow LED
-  // otherwise.
-  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
-  am_devices_led_off(am_bsp_psLEDs, AM_BSP_LED_GREEN);
-  if (person_score > no_person_score) {
-    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_GREEN);
-  } else {
-    am_devices_led_on(am_bsp_psLEDs, AM_BSP_LED_YELLOW);
-  }
+  ResetLeds();
 
-  TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
-                       person_score, no_person_score);
+  PrintDebugHeader(error_reporter, person_score, no_person_score);
+
+  // Create a value indicating which of the two classes has the higher confidence level.
+  //
+  // Positive values indicate a higher confidence for the person case, negative values
+  // a higher confidence for the no-person case.
+  int16_t prediction = person_score - no_person_score;
+
+  ProcessPrediction(error_reporter, prediction);
+  PrintDebugFooter(error_reporter, prediction);
 }
 
 #endif  // ARDUINO_EXCLUDE_CODE
